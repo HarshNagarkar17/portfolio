8bd89a682746902094879c428bf0a107785ce434
Harsh Nagarkar <harshvn187@gmail.com>
2023-11-26 19:10:19 +0530

    fixes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,8 +4,16 @@ export const Nav = () => {
   return (
     <nav>
         <div className="header">
-            <a href="">Contact Me <i className='bx bx-user'></i></a>
+            {/* <a href="Harsh_Nagarkar_Resume.pdf" download={true}>Download Resume <i class='bx bx-download'></i></a> */}
+            <div className="social-media-icons">
+                    <a href="https://www.linkedin.com/in/harsh-nagarkar/" target="_blank"><i className='bx bxl-linkedin'></i></a>
+                    <a href="https://twitter.com/hnx246" target="_blank"><i className='bx bxl-twitter'></i></a>
+                    <a href="https://github.com/HarshNagarkar17/" target="_blank"><i className='bx bxl-github'></i></a>
+                </div>
         </div>
+
+        {/* <div className="header-modal">
+        </div> */}
     </nav>
   )
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,13 +5,13 @@ export const Profile = () => {
             <div className="row-left">
                 <div className="profile-info">
                     <h3>Harsh Nagarkar</h3>
-                    <p>Aspring FullStack Developer</p>
+                    <p>Aspring Software Developer</p>
                 </div>
-                <div className="social-media-icons">
+                {/* <div className="social-media-icons">
                     <a href="https://www.linkedin.com/in/harsh-nagarkar/" target="_blank"><i className='bx bxl-linkedin'></i></a>
                     <a href="https://twitter.com/hnx246" target="_blank"><i className='bx bxl-twitter'></i></a>
                     <a href="https://github.com/HarshNagarkar17/" target="_blank"><i className='bx bxl-github'></i></a>
-                </div>
+                </div> */}
             </div>
         </div>
     )
